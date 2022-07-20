dd23a7cb6792705908471c262fc7785076b1bf4c
Monster Guo <68947448+gzy2570713180@users.noreply.github.com>
2022-07-20 23:03:57 +0800

    update marajo/array solution

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # algorithm
+
+the marajo is a island name, because I can't think of a good name, so I use the isLand name, may be I will also use in the future, but I will give a prompt.
+
+The marajo folder
+1.  Q: receive an int number than reverse it
--- marajo/src/array/ReverseNumber.java
+++ marajo/src/array/ReverseNumber.java
@@ -8,15 +8,16 @@ package array;
 
 /**
  * give any num , to reverse it, if it is a negative number, keep the minus sign
- * if it overflows Integer_MAX_VALUE , it should be zero
+ * if it overflows Integer scope , it should be zero
  */
 public class ReverseNumber {
 
-    //violent solution
     /**
+     * violent solution
+     * Time complexity: O(n)
+     * Space complexity: O(n)
      * convert this number to string, then to char array, reverse it,  pay attention to detail
      */
-
     public static int solutionOne(int x) {
         //eliminate some of the answers that are already clear
         if (x == Integer.MAX_VALUE || x == Integer.MIN_VALUE || x == 0){
@@ -34,7 +35,72 @@ public class ReverseNumber {
         return ((value > Integer.MAX_VALUE) ? 0 : (int) value) * sign;
     }
 
+
+    /**
+     * swap element in a array to lower the space overhead
+     *
+     * @param x receive a number to reverse
+     * @return answer
+     */
+    public static int solutionTwo(int x) {
+        //eliminate some of the answers that are already clear
+        if (x == Integer.MAX_VALUE || x == Integer.MIN_VALUE || x == 0){
+            return 0;
+        }
+        // get the sign
+        int sign = x > 0 ? 1 : -1;
+        // avoid the sign to interference calculation
+        int operateNumber = Math.abs(x);
+        //convert x to char array
+        char[] chars = String.valueOf(operateNumber).toCharArray();
+        //swap the start position num and end position num
+        for (int start = 0, end = chars.length - 1; start < end; start++, end--) {
+            char temp = chars[start];
+            chars[start] = chars[end];
+            chars[end] = temp;
+        }
+        //convert chars array to string
+        String s = String.valueOf(chars);
+        // convert s to long type, to prevent it overflow the limit of int type
+        long value = Long.parseLong(s);
+        // if it overflows, return zero, otherwise return the value convert to int;
+        return ((value > Integer.MAX_VALUE) ? 0 : (int) value) * sign;
+    }
+
+    /**
+     *
+     * By mathematical method to solve the problem
+     *
+     * @param x receive a number to reverse
+     * @return answer
+     */
+    public static int solutionThree(int x) {
+        //eliminate some of the answers that are already clear
+        if (x == Integer.MAX_VALUE || x == Integer.MIN_VALUE || x == 0){
+            return 0;
+        }
+        // get the sign
+        int sign = x > 0 ? 1 : -1;
+        // avoid the sign to interference calculation
+        int operateNumber = Math.abs(x);
+        // define the result
+        long result = 0;
+        // declare the unit digit
+        int last;
+        while ((last = operateNumber % 10) != operateNumber) {
+            result = result * 10 + last;
+            operateNumber /= 10;
+        }
+        // the highest digit num will not operate, so do it
+        if (operateNumber != 0) {
+            last = operateNumber;
+            result = result * 10 + last;
+        }
+        // if it overflows, return zero, otherwise return the value convert to int;
+        return ((result > Integer.MAX_VALUE) ? 0 : (int) result) * sign;
+    }
+
     public static void main(String[] args) {
-        System.out.println(solutionOne(120));
+        System.out.println(solutionThree(-1200));
     }
 }
